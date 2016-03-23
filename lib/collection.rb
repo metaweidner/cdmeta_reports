f77b2c7938c36c779ba74ea353f150b07a8b084a
Andrew Weidner <metaweidner@gmail.com>
2016-03-23 14:43:36 -0500

    refactor collection class

--- lib/collection.rb
+++ lib/collection.rb
@@ -1,42 +1,39 @@
 class Collection
 
-  attr_reader :map, :long_title, :title, :items, :labels_and_nicks
+  attr_reader :map, :long_title, :title, :items
 
-  def initialize(collection_alias, cdm_url, meta_map, collection_long_titles, collection_titles)
-    # get field information and map to dc
-    field_info = get_field_info(cdm_url, collection_alias)
-    @labels_and_nicks = get_labels_and_nicks(field_info)
-    @map = get_collection_map(labels_and_nicks, meta_map)
-
+  def initialize(collection_alias, repository)
+    # get field information and map to config file
+    field_info = get_field_info(repository.cdm_url, collection_alias)
+    labels_and_nicks = get_labels_and_nicks(field_info)
+    @map = get_collection_map(labels_and_nicks, repository.meta_map)
     # get titles
-    @long_title = collection_long_titles.fetch(collection_alias)
-    @title = collection_titles.fetch(collection_alias)
-
+    @long_title = repository.collection_long_titles.fetch(collection_alias)
+    @title = repository.collection_titles.fetch(collection_alias)
     #get items
-    @items = get_items(cdm_url, collection_alias)
-
+    @items = get_items(repository.cdm_url, collection_alias)
   end
 
-  # returns JSON from cdm api: dmGetCollectionFieldInfo
+  # returns hash from cdm api: dmGetCollectionFieldInfo
   def get_field_info(cdm_url, collection_alias)
     cdm_field_info_url = cdm_url + "dmGetCollectionFieldInfo/#{collection_alias}/json"
     field_info = JSON.parse(open(cdm_field_info_url).read)
   end
 
-  # returns field labels and cdm nicks in hash
+  # returns field labels and cdm nicks hash
   def get_labels_and_nicks(field_info)
     labels_and_nicks = {}
     field_info.each { |field| labels_and_nicks.store(field['name'], field['nick']) }
     labels_and_nicks
   end
 
-  # returns JSON from cdm api: dmQuery
+  # returns items hash from cdm api: dmQuery
   def get_items(cdm_url, collection_alias)
     cdm_items_url = cdm_url + "dmQuery/#{collection_alias}/0/title/title/2000/1/0/0/0/0/0/0/json"
     items = JSON.parse(open(cdm_items_url).read)
   end
 
-  # returns field mapping hash based on local YAML configuration file
+  # returns field mapping hash based on config file
   def get_collection_map(labels_and_nicks, meta_map)
     collection_map = {}
     field_map = {}
