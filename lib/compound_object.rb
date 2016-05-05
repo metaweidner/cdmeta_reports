fcab1cd2d8fb50ffffa18a3fbad7202a281953e5
Andrew Weidner <metaweidner@gmail.com>
2016-05-05 14:04:03 -0500

    update objects passed to compound_object class

--- lib/compound_object.rb
+++ lib/compound_object.rb
@@ -2,17 +2,18 @@ class CompoundObject
 
   attr_reader :items
 
-  def initialize(cdm_url, collection_alias, pointer)
-    info = get_compound_object_info(cdm_url, collection_alias, pointer)
+  def initialize(repository, collection, pointer)
+    info = get_compound_object_info(repository, collection, pointer)
     @items = get_compound_object_items(info)
   end
 
   # cdm api XML: dmGetCompoundObjectInfo
-  def get_compound_object_info(cdm_url, collection_alias, pointer)
-    cdm_compound_object_info_url = cdm_url + "dmGetCompoundObjectInfo/#{collection_alias}/#{pointer}/xml"
+  def get_compound_object_info(repository, collection, pointer)
+    cdm_compound_object_info_url = repository.cdm_url + "dmGetCompoundObjectInfo/#{collection.alias}/#{pointer}/xml"
     compound_object_info = Nokogiri::XML(open(cdm_compound_object_info_url))
   end
 
+  # parse dmGetCompoundObjectInfo XML
   def get_compound_object_items(info)
     compound_object_items = info.xpath("//pageptr/text()")
   end
