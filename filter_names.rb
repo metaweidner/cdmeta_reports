9291dccf3812a74293a4f622d9f91d2499fc2e79
Andrew Weidner <metaweidner@gmail.com>
2016-07-11 11:16:58 -0500

    update filter names

--- filter_names.rb
+++ filter_names.rb
@@ -6,7 +6,7 @@ ulan = []
 local = []
 co_creator = []
 
-# pass in the cdmeta_harvest file name
+# pass in the cdmeta_harvest file path
 # as command line argument
 File.open(ARGV[0], 'r') do |f|
 
@@ -15,12 +15,12 @@ File.open(ARGV[0], 'r') do |f|
 
     # split the line on the tabs
     data = line.split("\t")
-    # collection_name = data[0]
+    collection_name = data[0]
     # collection_alias = data[1]
     # object_pointer = data[2]
     # item_pointer = data[3]
     field_label = data[4]
-    value = data[5]
+    value = data[5].chomp("\n")
 
     # write unique values to vocab arrays
     if (field_label == 'creator_lcnaf') || \
@@ -30,7 +30,7 @@ File.open(ARGV[0], 'r') do |f|
       (field_label == 'artist_lcnaf') || \
       (field_label == 'composer_lcnaf') || \
       (field_label == 'added_composer_lcnaf')
-      lcnaf << value unless lcnaf.include? value
+      lcnaf << [value, field_label, collection_name]
     elsif (field_label == 'creator_ulan') || \
       (field_label == 'contributor_ulan') || \
       (field_label == 'subj_name_ulan') || \
@@ -38,7 +38,7 @@ File.open(ARGV[0], 'r') do |f|
       (field_label == 'artist_ulan') || \
       (field_label == 'composer_ulan') || \
       (field_label == 'added_composer_ulan')
-      ulan << value unless ulan.include? value
+      ulan << [value, field_label, collection_name]
     elsif (field_label == 'creator_hot') || \
       (field_label == 'contributor_hot') || \
       (field_label == 'subj_name_hot') || \
@@ -46,7 +46,7 @@ File.open(ARGV[0], 'r') do |f|
       (field_label == 'artist_hot') || \
       (field_label == 'composer_hot') || \
       (field_label == 'added_composer_hot')
-      hot << value unless hot.include? value
+      hot << [value, field_label, collection_name]
     elsif (field_label == 'creator_local') || \
       (field_label == 'contributor_local') || \
       (field_label == 'subj_name_local') || \
@@ -54,23 +54,43 @@ File.open(ARGV[0], 'r') do |f|
       (field_label == 'artist_local') || \
       (field_label == 'composer_local') || \
       (field_label == 'added_composer_local')
-      local << value unless local.include? value
+      local << [value, field_label, collection_name]
     else
-      co_creator << value unless co_creator.include? value
+      co_creator << [value, field_label, collection_name]
     end
   end
+end
 
-  # write files for each vocab with sorted unique values
-  File.open(File.join(__dir__, "lcnaf_names_#{Time.now.strftime("%Y%m%d_%k%M%S")}.txt"), 'w') do |f|
-    lcnaf.sort.each {|element| f.puts(element) }
+# write files for each vocab with sorted unique values
+File.open(File.join(__dir__, "lcnaf_names_#{Time.now.strftime("%Y%m%d_%k%M%S")}.txt"), 'w') do |f|
+  lcnaf.sort_by! { |e| e[0] }
+  lcnaf.uniq.each do |heading|
+    line = ""
+    heading.each { |h| line << h + "\t" }
+    f.write(line.chomp("\t") + "\n")
   end
-  File.open(File.join(__dir__, "ulan_names_#{Time.now.strftime("%Y%m%d_%k%M%S")}.txt"), 'w') do |f|
-    ulan.sort.each {|element| f.puts(element) }
+end
+File.open(File.join(__dir__, "ulan_names_#{Time.now.strftime("%Y%m%d_%k%M%S")}.txt"), 'w') do |f|
+  ulan.sort_by! { |e| e[0] }
+  ulan.uniq.each do |heading|
+    line = ""
+    heading.each { |h| line << h + "\t" }
+    f.write(line.chomp("\t") + "\n")
   end
-  File.open(File.join(__dir__, "hot_names_#{Time.now.strftime("%Y%m%d_%k%M%S")}.txt"), 'w') do |f|
-    hot.sort.each {|element| f.puts(element) }
+end
+File.open(File.join(__dir__, "hot_names_#{Time.now.strftime("%Y%m%d_%k%M%S")}.txt"), 'w') do |f|
+  hot.sort_by! { |e| e[0] }
+  hot.uniq.each do |heading|
+    line = ""
+    heading.each { |h| line << h + "\t" }
+    f.write(line.chomp("\t") + "\n")
   end
-  File.open(File.join(__dir__, "local_names_#{Time.now.strftime("%Y%m%d_%k%M%S")}.txt"), 'w') do |f|
-    local.sort.each {|element| f.puts(element) }
+end
+File.open(File.join(__dir__, "local_names_#{Time.now.strftime("%Y%m%d_%k%M%S")}.txt"), 'w') do |f|
+  local.sort_by! { |e| e[0] }
+  local.uniq.each do |heading|
+    line = ""
+    heading.each { |h| line << h + "\t" }
+    f.write(line.chomp("\t") + "\n")
   end
 end
